df7fb3a30188c9f66193dc99d2524b65d2183a96
Oualid Sellal <oualid.sellal@gmail.com>
2020-10-27 09:08:47 +0100

    update

--- packages/infrastructure/package.js
+++ packages/infrastructure/package.js
@@ -1,6 +1,1 @@
-/* eslint-disable global-require */
-if (process.env.NODE_ENV === 'PROD') {
-	module.exports = require('./dist/index.js');
-} else {
-	module.exports = require('./src/index.js');
-}
+module.exports = require('./dist/index.js');
