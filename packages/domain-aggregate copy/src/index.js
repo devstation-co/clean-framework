5ffab905f704ab9de1f0ed07d427c524632dbf63
Oualid Sellal <oualid.sellal@gmail.com>
2020-11-05 19:54:09 +0100

    update domain-event

---
+++ packages/domain-aggregate copy/src/index.js
@@ -0,0 +1,119 @@
+import Validator from '@devstation.co/validator.infrastructure.micromodule';
+
+export default class BaseAggregate {
+	#aggregateType;
+
+	#aggregateId;
+
+	#state;
+
+	#validator;
+
+	#events;
+
+	#entities;
+
+	#eventStore;
+
+	constructor({ id, type, events, entities, eventStore }) {
+		this.init({ id, type, events, entities });
+		if (!eventStore) throw new Error('Event-store undefined');
+		this.#eventStore = eventStore;
+		this.#validator = new Validator();
+	}
+
+	init({ id, type, events, entities }) {
+		if (!id) throw new Error('Aggregate id undefined');
+		if (!type) throw new Error('Aggregate type undefined');
+		this.#aggregateId = id;
+		this.#aggregateType = type;
+		this.#state = {
+			id,
+		};
+		this.#events = events;
+		this.#entities = entities;
+
+		entities.forEach((Entity) => {
+			const entity = new Entity({
+				aggregate: {
+					id,
+					type,
+				},
+			});
+			const entityType = entity.getEntityType();
+			this.#events = { ...this.#events, ...entity.getEvents() };
+			this.#entities[`${entityType}`] = entity;
+		});
+	}
+
+	async validate({ params, schema }) {
+		const res = await this.#validator.validate({ data: params, schema });
+		return res;
+	}
+
+	getAggregateId() {
+		return this.#aggregateId;
+	}
+
+	getAggregateType() {
+		return this.#aggregateType;
+	}
+
+	getState() {
+		return this.#state;
+	}
+
+	async generateEvent({ type, meta, payload }) {
+		const eventType = `${type.charAt(0).toUpperCase()}${type.slice(1)}`;
+		const Event = this.#events[`${eventType}`];
+		const event = new Event({
+			aggregate: {
+				id: this.getAggregateId(),
+				type: this.getAggregateType(),
+			},
+		});
+		await event.init({ meta, payload });
+		return event;
+	}
+
+	async hydrate() {
+		const events = await this.#eventStore.getEventsByAggregateId({
+			aggregateId: this.#aggregateId,
+		});
+		for (let index = 0; index < events.length; index += 1) {
+			const event = events[`${index}`];
+			const eventType = `${event.type.charAt(0).toUpperCase()}${event.type.slice(1)}`;
+			if (typeof this.#events[`${eventType}`] === 'function') {
+				const Event = this.#events[`${eventType}`];
+				const constructedEvent = new Event({
+					aggregate: event.aggregate,
+				});
+				// eslint-disable-next-line no-await-in-loop
+				await constructedEvent.init({
+					timestamp: event.timestamp,
+					meta: event.meta,
+					payload: event.payload,
+				});
+				this.#state = constructedEvent.apply({ state: this.#state });
+			}
+		}
+		return true;
+	}
+
+	async commit({ event }) {
+		this.apply({ event });
+		const eventDetails = event.getDetails();
+		await this.#eventStore.commit({ event: eventDetails });
+		return true;
+	}
+
+	apply({ event }) {
+		this.#state = event.apply({ state: this.#state });
+	}
+
+	getEntity({ type }) {
+		const entity = this.#entities[`${type}`];
+		if (!entity) throw new Error(`Entity ${type} undefined`);
+		return entity;
+	}
+}
--- packages/domain-event/src/index.js
+++ packages/domain-event/src/index.js
@@ -1,50 +1,65 @@
-export default class BaseEvent {
-	#id;
+import Validator from '@devstation.co/validator.infrastructure.micromodule';
+
+export default class Base {
+	#validator;
 
 	#type;
 
+	#aggregateId;
+
+	#aggregateType;
+
+	#payload = {};
+
+	#meta = {};
+
 	#timestamp;
 
-	#meta;
+	#metaSchema;
 
-	#aggregate;
+	#payloadSchema;
 
-	#payload;
-
-	constructor({ id, type, timestamp, meta, aggregate, aggregateId, aggregateType, payload }) {
+	constructor({ type, aggregate, payload, meta }) {
+		this.#validator = new Validator();
 		if (!type) throw new Error('Event type undefined');
-		if (aggregate) {
-			if (!aggregate.type) throw new Error('Aggregate type undefined');
-			if (!aggregate.id) throw new Error('Aggregate type undefined');
-			this.#aggregate = {
-				id: aggregate.id,
-				type: aggregate.type,
-			};
-		} else {
-			if (!aggregateType) throw new Error('Aggregate type undefined');
-			if (!aggregateId) throw new Error('Aggregate id undefined');
-			this.#aggregate = {
-				id: aggregateId,
-				type: aggregateType,
-			};
-		}
-		this.#id = id;
+		if (!aggregate?.id) throw new Error('Aggregate id undefined');
+		if (!aggregate?.type) throw new Error('Aggregate type undefined');
+		if (!payload) throw new Error('Payload schema undefined');
+		if (!meta) throw new Error('Meta schema undefined');
 		this.#type = type;
-		this.#timestamp = timestamp || new Date();
-		this.#meta = meta || {};
-
-		this.#payload = payload || {};
+		this.#aggregateId = aggregate.id;
+		this.#aggregateType = aggregate.type;
+		this.#metaSchema = meta;
+		this.#payloadSchema = payload;
 	}
 
 	getDetails() {
-		const event = {
-			id: this.#id,
+		const details = {
 			type: this.#type,
 			timestamp: this.#timestamp,
-			aggregate: { ...this.#aggregate },
+			aggregate: {
+				id: this.#aggregateId,
+				type: this.#aggregateType,
+			},
 			meta: { ...this.#meta },
 			payload: { ...this.#payload },
 		};
-		return event;
+		return Object.freeze(details);
+	}
+
+	async init({ timestamp, meta = {}, payload = {} }) {
+		if (!meta) throw new Error('Event meta undefined');
+		if (!payload) throw new Error('Event payload undefined');
+		this.#timestamp = timestamp || new Date();
+		await this.#validator.validate({
+			data: meta,
+			schema: this.#metaSchema,
+		});
+		await this.#validator.validate({
+			data: payload,
+			schema: this.#payloadSchema,
+		});
+		this.#payload = { ...payload };
+		this.#meta = { ...meta };
 	}
 }
